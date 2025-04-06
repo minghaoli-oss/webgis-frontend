72b4c7f96585e784d0bf4147b07a6ff5ea196a62
minghaoli-oss <a13977565834@gmail.com>
2025-04-06 10:03:15 +0800

    路由报错解决

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,26 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 
-// 导入页面组件（注意路径大小写敏感）
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('../views/Home.vue')  // 正确路径写法
+    component: () => import('@/views/Home.vue')  // 注意大小写
   },
   {
     path: '/data',
     name: 'Data',
-    component: () => import('../views/DataManagement.vue')
+    component: () => import('@/views/DataManagement.vue')
   },
-  {
+  {  // 新增map路由配置
     path: '/map',
     name: 'Map',
-    component: () => import('../views/MapView.vue')
+    component: () => import('@/views/MapView.vue')  // 确保路径正确
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes
 })
 
